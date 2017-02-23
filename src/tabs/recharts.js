5fc13659900df554c8fc00488c94344bb00ed2e1
许妍 <chrisvy@126.com>
2017-02-23 11:09:20 +0800

    chart

--- src/tabs/recharts.js
+++ src/tabs/recharts.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import CustomLineDot from './CustomLineDot';
 // import { changeNumberOfData } from './utils';
 import { ResponsiveContainer, LineChart, Line, XAxis, YAxis, ReferenceLine,
   ReferenceDot, Tooltip, CartesianGrid, Legend, Brush, ErrorBar } from 'recharts';
@@ -18,60 +17,9 @@ const data = [
   { name: 'Page J', uv: 189, pv: 4800, amt: 2400, uvError: [15, 60] },
 ];
 
-const data01 = [
-  { day: '05-01', weather: 'sunny' },
-  { day: '05-02', weather: 'sunny' },
-  { day: '05-03', weather: 'cloudy' },
-  { day: '05-04', weather: 'rain' },
-  { day: '05-05', weather: 'rain' },
-  { day: '05-06', weather: 'cloudy' },
-  { day: '05-07', weather: 'cloudy' },
-  { day: '05-08', weather: 'sunny' },
-  { day: '05-09', weather: 'sunny' },
-];
-
-const data02 = [
-  { name: 'Page A', uv: 300, pv: 2600, amt: 3400 },
-  { name: 'Page B', uv: 400, pv: 4367, amt: 6400 },
-  { name: 'Page C', uv: 300, pv: 1398, amt: 2400 },
-  { name: 'Page D', uv: 200, pv: 9800, amt: 2400 },
-  { name: 'Page E', uv: 278, pv: 3908, amt: 2400 },
-  { name: 'Page F', uv: 189, pv: 4800, amt: 2400 },
-  { name: 'Page G', uv: 189, pv: 4800, amt: 2400 },
-];
-
 const initilaState = {
-  data,
-  data01,
-  data02,
-  opacity: 1,
-  anotherState: false,
+  data
 };
-
-const renderSpecialDot = (props) => {
-  const { cx, cy, stroke, key } = props;
-
-  if (cx === +cx && cy === +cy) {
-    return <path d={`M${cx - 2},${cy - 2}h4v4h-4Z`} fill={stroke} key={key}/>;
-  }
-
-  return null;
-};
-
-const renderLabel = (props) => {
-  const { x, y, textAnchor, key, value } = props;
-
-  if (x === +x && y === +y) {
-    return <text x={x} y={y} dy={-10} textAnchor={textAnchor} key={key}>{value}</text>
-  }
-
-  return null;
-};
-
-const scale = scaleLog().base(10).nice();
-const specifiedDomain = [0.01, 'auto'];
-const specifiedTicks = [0.01, 0.1, 1, 10, 100, 1000];
-const specifiedMargin = { top: 20, right: 20, bottom: 20, left: 20 };
 
 export default React.createClass({
   displayName: 'LineChartDemo',
@@ -80,46 +28,15 @@ export default React.createClass({
     return initilaState;
   },
 
-  handleChangeData() {
-    // this.setState(() => _.mapValues(initilaState, changeNumberOfData));
-  },
-
   handleClick(data, e) {
     console.log(data);
   },
 
-  handleLegendMouseEnter() {
-    this.setState({
-      opacity: 0.5,
-    });
-  },
-
-  handleLegendMouseLeave() {
-    this.setState({
-      opacity: 1,
-    });
-  },
-
-  handleChangeAnotherState() {
-    this.setState({
-      anotherState: !this.state.anotherState,
-    })
-  },
-
   render() {
-    const { data, data01, data02, opacity } = this.state;
+    const { data } = this.state;
 
     return (
       <div className='line-charts'>
-        <a
-          href="javascript: void(0);"
-          className="btn update"
-          onClick={this.handleChangeData}
-        >
-          change data
-        </a>
-        <button onClick={this.handleChangeAnotherState}>switch another state</button>
-        <br/>
 
         <p>LineChart with two y-axes</p>
         <div className='line-chart-wrapper' style={{ padding: 40 }}>
@@ -131,25 +48,11 @@ export default React.createClass({
             onClick={this.handleClick}
           >
             <XAxis dataKey='name' interval="preserveStartEnd" />
+            <XAxis />
             <Tooltip/>
             <CartesianGrid stroke='#f5f5f5'/>
             <Line type='monotone' dataKey='uv' stroke='#ff7300' yAxisId={0} activeDot={{fill: '#ff7300', stroke: 'none'}}/>
             <Line type='monotone' dataKey='pv' stroke='#387908' yAxisId={1} activeDot={{fill: '#387908', stroke: 'none', r: 6}}/>
-          </LineChart>
-        </div>
-
-        <p>LineChart with three y-axes</p>
-        <div className='line-chart-wrapper' style={{ margin: 40 }}>
-          <LineChart width={600} height={400} data={data}>
-            <YAxis type='number' yAxisId={0} ticks={[0, 250]}/>
-            <YAxis type='number' orientation='right' yAxisId={1}/>
-            <YAxis type='number' orientation='right' yAxisId={2}/>
-            <XAxis dataKey='name'/>
-            <Tooltip position={{y: 200}} />
-            <CartesianGrid stroke='#f5f5f5'/>
-            <Line dataKey='uv' stroke='#ff7300' strokeWidth={2} yAxisId={0}/>
-            <Line dataKey='pv' stroke='#387908' strokeWidth={2} yAxisId={1}/>
-            <Line dataKey='amt' stroke='#38abc8' strokeWidth={2} yAxisId={2}/>
           </LineChart>
         </div>
       </div>
